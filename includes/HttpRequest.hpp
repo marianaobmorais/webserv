7ef0d5bd09b219fa7a3ca32d57b2172eb3d0ecfd
Jose Junior <jose_evilasio@outlook.com>
2025-08-10 17:22:34 +0100

    changed to DTO

--- includes/HttpRequest.hpp
+++ includes/HttpRequest.hpp
@@ -2,54 +2,48 @@
 #define HTTP_REQUEST_HPP
 
 #include <string>
-#include <sstream>
-#include <iostream>
-#include <cstdlib>
-#include <map>
-#include <vector>
 
-struct HttpMethod
-{
-	enum Method
-	{
-		GET = 0, POST, DELETE, PUT, INVALID
-	};
-};
+//webserv
+#include <HttpMethod.hpp>
+#include <RequestHeader.hpp>
+#include <RequestMeta.hpp>
+#include <RequestParseError.hpp>
 
+//Data Transfer Object
 class HttpRequest
 {
 	private:
-
-		std::map<std::string, std::string>	_headers;
-		HttpMethod::Method					_method;
-		std::string							_uri;
-		int									_major;
-		int									_minor;
+		HttpMethod::Method			_method;
+		std::string					_uri;
+		int							_major;
+		int							_minor;
+		RequestHeader				_headers;
+		RequestMeta					_meta;
+		std::string					_bodyRef;
+		RequestParseError::reason	_parseError;
 
 		//setters
-		void	setHeaders(std::map<std::string, std::string> headers);
-		void	setMethod(const std::string& method);
-		void	setUri(std::string uri);
+		void	setMethod(const HttpMethod::Method& method);
+		void	setUri(const std::string& uri);
 		void	setMajor(int major);
 		void	setMinor(int minor);
-
-		//parser
-		std::vector<std::string>	split(const std::string& str, const std::string& sep);
-		void						saveRequestLine(const std::string& str);
+		void	setHeaders(const RequestHeader& headers);
+		void	setMeta(const RequestMeta& meta);
+		void	setBodyRef(const std::string& body_ref);
+		void	setParseError(RequestParseError::reason reason);
 
 	public:
-
 		HttpRequest(const std::string& request);
 		~HttpRequest();
-		//add copy assignment?
-		//add copy?
 
 		//getters
-		const std::map<std::string, std::string>&	getHeaders(void) const;
-		HttpMethod::Method							getMethod(void) const; //convert string?
-		const std::string&							getUri(void) const;
-		int											getMajor(void) const;
-		int											getMinor(void) const;
+		HttpMethod::Method			getMethod(void) const;
+		const std::string&			getUri(void) const;
+		int*						getHttpVersion(void) const;
+		const RequestHeader&		getHeaders(void) const;
+		const RequestMeta&			getMeta(void) const;
+		const std::string&			getBodyRef(void) const;
+		RequestParseError::reason	getParseError(void) const;
 };
 
 #endif //HTTP_REQUEST_HPP
--- srcs/HttpRequest.cpp
+++ srcs/HttpRequest.cpp
@@ -1,78 +1,38 @@
-#include "../includes/HttpRequest.hpp"
+#include <HttpRequest.hpp>
 
-HttpRequest::HttpRequest(const std::string& request)
-{
-	std::vector<std::string> lines = HttpRequest::split(request, "\r\n");
-	HttpRequest::saveRequestLine(lines[0]);
-
-	// for (size_t i = 0; i < lines.size(); ++i)
-	// {
-	// 	std::cout << lines[i] << std::endl;
-	// }
-}
+HttpRequest::HttpRequest(const std::string& request) {}
 
 HttpRequest::~HttpRequest() {}
 
-std::vector<std::string>	HttpRequest::split(const std::string& str, const std::string& sep)
+void	HttpRequest::setMethod(const HttpMethod::Method& method)
 {
-	std::vector<std::string>	tokens;
-	size_t						start = 0;
-	size_t						end = str.find(sep);
-
-	while (end != std::string::npos)
-	{
-		tokens.push_back(str.substr(start, end - start));
-		start = end + sep.length();
-		end = str.find(sep, start);
-	}
-	tokens.push_back(str.substr(start));
-	return (tokens);
+	this->_method = method;
 }
 
-void	HttpRequest::saveRequestLine(const std::string& str)
-{
-	std::vector<std::string> requestLine = HttpRequest::split(str, " ");
-
-	HttpRequest::setMethod(requestLine[0]);
-	HttpRequest::setUri(requestLine[1]);
-
-	std::vector<std::string> httpVersion = HttpRequest::split(requestLine[2].substr(5), ".");
-
-	HttpRequest::setMajor(std::atoi(httpVersion[0].c_str()));
-	HttpRequest::setMinor(std::atoi(httpVersion[1].c_str()));
-}
-
-void	HttpRequest::setHeaders(std::map<std::string, std::string> headers)
-{
-	this->_headers = headers;
-}
-
-void	HttpRequest::setMethod(const std::string& method)
-{
-	if (method == "GET")
-		this->_method = HttpMethod::GET;
-	else if (method == "POST")
-		this->_method = HttpMethod::POST;
-	else if (method == "DELETE")
-		this->_method = HttpMethod::DELETE;
-	else if (method == "PUT")
-		this->_method = HttpMethod::PUT;
-	else
-		this->_method = HttpMethod::INVALID;
-}
-
-void	HttpRequest::setUri(std::string uri)
-{
-	this->_uri = uri;
-}
+void	HttpRequest::setUri(const std::string& uri) { this->_uri = uri; }
 
 void	HttpRequest::setMajor(int major) { this->_major = major; }
 
 void	HttpRequest::setMinor(int minor) { this->_minor = minor; }
 
-const std::map<std::string, std::string>&	HttpRequest::getHeaders(void) const
+void	HttpRequest::setHeaders(const RequestHeader& headers)
 {
-	return (this->_headers);
+	this->_headers = headers;
+}
+
+void	HttpRequest::setMeta(const RequestMeta& meta)
+{
+	this->_meta = meta;
+}
+
+void	HttpRequest::setBodyRef(const std::string& body_ref)
+{
+	this->_bodyRef = body_ref;
+}
+
+void	HttpRequest::setParseError(RequestParseError::reason reason)
+{
+	this->_parseError = reason;
 }
 
 HttpMethod::Method	HttpRequest::getMethod(void) const
@@ -80,11 +40,31 @@ HttpMethod::Method	HttpRequest::getMethod(void) const
 	return (this->_method);
 }
 
-const std::string&	HttpRequest::getUri(void) const
+const std::string&	HttpRequest::getUri(void) const { return (this->_uri); }
+
+int*	HttpRequest::getHttpVersion(void) const
 {
-	return (this->_uri);
+	int version[2] = {this->_major, this->_minor};
+
+	return (version);
 }
 
-int	HttpRequest::getMajor(void) const { return (this->_major); }
+const RequestHeader&	HttpRequest::getHeaders(void) const
+{
+	return (this->_headers);
+}
 
-int	HttpRequest::getMinor(void) const { return (this->_minor); }
+const RequestMeta&	HttpRequest::getMeta(void) const
+{
+	return (this->_meta);
+}
+
+const std::string&	HttpRequest::getBodyRef(void) const
+{
+	return (this->_bodyRef);
+}
+
+RequestParseError::reason	HttpRequest::getParseError(void) const
+{
+	return (this->_parseError);
+}
