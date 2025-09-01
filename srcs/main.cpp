1b85fcab7e540decac1758013fafe1428eb36c00
Mariana Morais <mariaoli@student.42porto.com>
2025-09-01 19:59:10 +0100

    Working on main.cpp

---
+++ srcs/main.cpp
@@ -0,0 +1,24 @@
+#include "../includes/WebServer.hpp"
+#include "../includes/ServerSocket.hpp"
+#include <iostream>
+#include <string>
+
+int	main(int argc, char** argv)
+{
+	std::string	configFile;
+
+	if (argc > 2)
+	{
+		std::cout << "usage: ./webserv [config_file]" << std::endl;
+		return (1);
+	}
+	if (argc == 1)
+		configFile = "defautFile"; //create file and put it in the repository?
+	if (argc == 2)
+		configFile = argv[1];
+	std::cout << "Instatiate WebServer" << std::endl; //debug
+
+
+
+	return (0);
+}
