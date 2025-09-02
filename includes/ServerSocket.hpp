35aeec9979f76ba8a6b79515267806751815a439
Mariana <mariana.obm@gmail.com>
2025-09-02 18:45:27 +0100

    Added setFD()

--- includes/ServerSocket.hpp
+++ includes/ServerSocket.hpp
@@ -6,15 +6,18 @@
 class ServerSocket
 {
 	private:
-		int				_fd; // is it const? or a reference?
+		int				_fd;
 		ServerSocket(ServerSocket const& src);
 		ServerSocket&	operator=(ServerSocket const& rhs);
 	public:
-		ServerSocket(void); //should I accept non-parameterized constructor?
-		ServerSocket(int newFD);
+		ServerSocket(void);
 		~ServerSocket(void);
 
-		void			listen(/* ip, port */); //bind + listen
+		//accessors
+		void			setFD(int newFD);
+
+		//later
+		//void			listen(/* ip, port */); //bind + listen
 		//ClientConnection	accept(void); //accepts clients, producing ClientConnection
 };
 
