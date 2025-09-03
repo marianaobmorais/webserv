52e4874ec1cd0202fd91552e2d060be1b223f4c1
Mariana <mariana.obm@gmail.com>
2025-09-03 09:54:41 +0100

    Added close to destructor

--- srcs/ClientConnection.cpp
+++ srcs/ClientConnection.cpp
@@ -1,5 +1,10 @@
 #include "../includes/ClientConnection.hpp"
+#include <unistd.h> //close() 
 
 ClientConnection::ClientConnection(int fd) : _fd(fd) {}
 
-ClientConnection::~ClientConnection(void) {}
+ClientConnection::~ClientConnection(void)
+{
+	if (this->_fd != -1)
+		::close(this->_fd);
+}
