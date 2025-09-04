642ceb806d5eb090ddb9c3db30c56279c9b27f6c
Mariana <mariana.obm@gmail.com>
2025-09-04 11:58:51 +0100

    Set clientFD to non-blocking

--- srcs/ClientConnection.cpp
+++ srcs/ClientConnection.cpp
@@ -1,7 +1,26 @@
 #include "../includes/ClientConnection.hpp"
-#include <unistd.h> //close() 
+#include <unistd.h> //close()
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <cstring> //strerror()
+#include <stdexcept>
+#include <string>
 
-ClientConnection::ClientConnection(int fd) : _fd(fd) {}
+//Set client sockets to non-blocking too. The new fd you get from accept() may not 
+//inherit non-blocking on all platforms. After accept(), call fcntl(clientFD, F_SETFL, O_NONBLOCK) 
+//(or have your ClientConnection do it in its constructor).
+ClientConnection::ClientConnection(int fd) : _fd(fd)
+{
+	int	clientFD;
+
+	if (::fcntl(clientFD, F_SETFL, O_NONBLOCK) == -1)
+	{
+		::close(clientFD);
+		std::string	errorMsg(strerror(errno));
+		throw std::runtime_error("error: fcntl: " + errorMsg);
+	}
+}
 
 ClientConnection::ClientConnection(ClientConnection const& src) : _fd(src._fd) {}
 
