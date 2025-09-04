323c3d15eacc92d3a8bfbd6d95116c67a6e6ffcc
Mariana <mariana.obm@gmail.com>
2025-09-04 12:41:48 +0100

    Removed fcntl from constructor

--- srcs/ClientConnection.cpp
+++ srcs/ClientConnection.cpp
@@ -7,20 +7,7 @@
 #include <stdexcept>
 #include <string>
 
-//Set client sockets to non-blocking too. The new fd you get from accept() may not 
-//inherit non-blocking on all platforms. After accept(), call fcntl(clientFD, F_SETFL, O_NONBLOCK) 
-//(or have your ClientConnection do it in its constructor).
-ClientConnection::ClientConnection(int fd) : _fd(fd)
-{
-	int	clientFD;
-
-	if (::fcntl(clientFD, F_SETFL, O_NONBLOCK) == -1)
-	{
-		::close(clientFD);
-		std::string	errorMsg(strerror(errno));
-		throw std::runtime_error("error: fcntl: " + errorMsg);
-	}
-}
+ClientConnection::ClientConnection(int fd) : _fd(fd) {}
 
 ClientConnection::ClientConnection(ClientConnection const& src) : _fd(src._fd) {}
 
