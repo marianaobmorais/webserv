711574b9effcd84d1afc9ef747433c73e0b31b89
Mariana <mariana.obm@gmail.com>
2025-09-05 13:56:08 +0100

    Implementes addToPollFD and removeClientConnection, refactored start() and run() to receive data from client

--- srcs/WebServer.cpp
+++ srcs/WebServer.cpp
@@ -1,8 +1,9 @@
 #include "../includes/WebServer.hpp"
 #include <sys/socket.h> // SOMAXCONN
-#include <cstring>
+#include <unistd.h> //close()
 #include <errno.h>
 #include <poll.h>
+#include <cstring>
 #include <stdexcept>
 #include <iostream>
 
@@ -10,17 +11,14 @@ WebServer::WebServer(void) : _serverSocket() {}
 
 WebServer::~WebServer(void){}
 
-void		WebServer::start(void)
+void	WebServer::start(void)
 {
 	this->_serverSocket.bindSocket("8080"); //this parameter will be from config file probably
 	this->_serverSocket.listenConnections(SOMAXCONN);
 
 	//start _pollFDs vector
-	struct pollfd	serverPollFD;
-	serverPollFD.fd = this->_serverSocket.getFD();
-	serverPollFD.events = POLLIN; // monitor for incoming connections
-	serverPollFD.revents = 0;
-	this->_pollFDs.push_back(serverPollFD); // the server's listening socket is always the first [0], the client ones start from index 1
+	// the server's listening socket is always the first [0], the client ones start from index 1
+	this->addToPollFD(this->_serverSocket.getFD(), POLLIN); // monitor for incoming connections
 }
 
 void	WebServer::run(void)
@@ -29,14 +27,17 @@ void	WebServer::run(void)
 	{
 		if (::poll(&this->_pollFDs[0], this->_pollFDs.size(), -1) == -1) //-1 means it won't time out
 		{
+			if (errno == EINTR) //are there other "harmless/temporary errors"?
+				continue ;
 			std::string	errorMsg(strerror(errno));
-			throw std::runtime_error("error: poll: " + errorMsg); //continue ; to keep the server alive on temporary errors like EINTR or throw?
+			throw std::runtime_error("error: poll: " + errorMsg);
 		}
+
+		//std::cout << "clients: " << _clients.size() << " pollFDs: " << _pollFDs.size() << std::endl; //debug
 
 		for (size_t i = 0; i < this->_pollFDs.size(); i++)  // Loop through all monitored FDs
 		{
-			//later: handle resize of this->_pollFDs here?
-			if (this->_pollFDs[i].revents & POLLIN) //POLLIN bit is set, regardless of what other bits may also be se
+			if (this->_pollFDs[i].revents & POLLIN) //check if POLLIN bit is set, regardless of what other bits may also be set
 			{
 				if (this->_pollFDs[i].fd == this->_serverSocket.getFD()) // Ready on listening socket -> accept new client
 				{
@@ -44,48 +45,69 @@ void	WebServer::run(void)
 					for (size_t j = 0; j < newFDs.size(); j++)
 					{
 						int	newClientFD = newFDs[j];
-						this->_clients.insert(std::make_pair(newClientFD, ClientConnection(newClientFD))); // saves the client object in _clients
+						if (_clients.find(newClientFD) == _clients.end()) //avoid duplicates
+						{
+							this->_clients.insert(std::make_pair(newClientFD, ClientConnection(newClientFD))); //should I create a method that saves the client object in _clients?
+							//std::cout << "populate client map with fd: " << newClientFD << std::endl; //debug
 
-						//Add to pollFDs
-						struct pollfd	newPollFD;
-						newPollFD.fd = newClientFD;
-						newPollFD.events = POLLIN; // monitor for incoming data
-						newPollFD.revents = 0;
-						this->_pollFDs.push_back(newPollFD); //adds the client’s file descriptor to _pollFDs so poll() will also monitor it
+							//Add to pollFDs //adds the client’s file descriptor to _pollFDs so poll() will also monitor it
+							this->addToPollFD(newClientFD, POLLIN);
+						}
 					}
 				}
 				else //If it wasn’t the server socket, then it must be one of the client sockets
 				{
-					//std::cout << "Client FD " << this->_pollFDs[i].fd << " has data ready" << std::endl;
+					//std::cout << "Try to read data from fd: " << _pollFDs[i].fd << std::endl; //debug
+
 					std::map<int, ClientConnection>::iterator	it;
 					it = this->_clients.find(_pollFDs[i].fd);
-					if (it != this->_clients.end()) //I don't understand this check
+					if (it != this->_clients.end()) //I don't understand this check //is there a chance that this check is false?
+					{
 						ClientConnection	&client = it->second;
+						try
+						{
+							ssize_t				bytesRecv = client.recvData();
 
-					char	buffer[1024];
-					ssize_t	bytesRecv = recv(_pollFDs[i].fd, buffer, sizeof(buffer), 0);
-
-					if (bytesRecv > 0)
-						//handle request
-					if (bytesRecv == 0)
-					{
-						; //client closed connection
+							if (bytesRecv > 0)
+							{
+								if (client.completedRequest())
+									//std::cout << "Handle HTTP parse" << std::endl; //TODO
+									std::cout << client.getRecvBuffer() << std::endl;
+							}
+							else if (bytesRecv == 0)
+							{
+								this->removeClientConnection(client.getFD(), i);
+								i--;
+							}
+						}
+						catch (std::exception const& e)
+						{
+							std::cerr << e.what() << '\n'; //error
+							this->removeClientConnection(client.getFD(), i);
+							i--;
+						}
 					}
-					else
-					{
-						if (errno == EAGAIN || errno == EWOULDBLOCK)
-							; //continue ; probably
-						else
-							; //client closed the connection
-					}
-					
-
 				}
 			}
 			else //!(this->_pollFDs[i].revents & POLLIN)
 			{
-				//later //how to handle resizing of this->_pollFDs?
+				; //later //how to handle resizing of this->_pollFDs?
 			}
 		}
 	}
+}
+
+void	WebServer::removeClientConnection(int clientFD, size_t poolFDIndex)
+{
+	this->_clients.erase(clientFD); //erase by key // the clientConnection destructor already handles close the fd -> RAII
+	this->_pollFDs.erase(_pollFDs.begin() + poolFDIndex); //erase by iterator position
+}
+
+void	WebServer::addToPollFD(int fd, short events)
+{
+	struct pollfd	pollFD;
+	pollFD.fd = fd;
+	pollFD.events = events;
+	pollFD.revents = 0;
+	this->_pollFDs.push_back(pollFD);
 }
