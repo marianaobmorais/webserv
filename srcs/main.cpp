19dcecaad7c3e5b79d3fe98aaffa4aff337fef02
Jose Junior <jose_evilasio@outlook.com>
2025-09-07 22:05:18 +0100

    update

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,7 +4,35 @@
 
 int	main(void)
 {
-	std::string	clientRequest = "GET /hello HTTP/1.1\r\nHost: localhost:8000\r\nUser-Agent: curl/7.68.0\r\nAccept: */*\r\n\r\n";
+	std::string	clientRequest = 
+	"POST /api/login HTTP/1.1\r\n"
+	"Host: localhost:8000\r\n"
+	"Content-Type: application/json\r\n"
+	"Content-Length: 49\r\n"
+	"Connection: close\r\n"
+	"\r\n"
+	"{\r\n"
+	"  \"username\": \"alice\",\r\n"
+	"  \"password\": \"12345\"\r\n"
+	"}\r\n";
 
+	HttpRequest request;
+	RequestParse::handleRawRequest(clientRequest, request);
+
+	if (request.getParseError() != RequestParseError::OK)
+		std::cout << "Error!" << std::endl;\
+	else
+	{
+		std::cout << "Method: " << request.getMethod() << std::endl;
+		std::cout << "URI: " << request.getUri() << std::endl;
+		std::cout << "HttpVersion: " << request.getHttpVersion()[0] << request.getHttpVersion()[1] << std::endl;
+		std::cout << " -- HttpRequest Meta -- " << std::endl;
+		std::cout << "Host: " << request.getMeta().getHost() << std::endl;
+		std::cout << "Content-Length: " << request.getMeta().getContentLength() << std::endl;
+		std::cout << "Tranfer-Encoding: " << request.getMeta().isChunked() << std::endl;
+		std::cout << "Connection close: " << request.getMeta().shouldClose() << std::endl;
+		std::cout << "Expect: " << request.getMeta().getExpectContinue() << std::endl;
+		std::cout << "Body: " << request.getBody() << std::endl;
+	}
 	return (0);
 }
