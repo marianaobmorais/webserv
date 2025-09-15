5c4ede78e870d680247fe9c93198bf23d73077d6
Mariana <mariana.obm@gmail.com>
2025-09-15 19:46:15 +0100

    Started setting up Config class

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,5 +1,6 @@
 #include "../includes/WebServer.hpp"
 #include "../includes/ServerSocket.hpp"
+#include "../includes/Config.hpp"
 #include <iostream>
 #include <string>
 
@@ -13,11 +14,12 @@ int	main(int argc, char** argv)
 		return (1);
 	}
 	if (argc == 1)
-		configFile = "defautFile"; //create file and put it in the repository?
+		configFile = "defaut.config"; //create file and put it in the repository?
 	if (argc == 2)
 		configFile = argv[1];
 	try
 	{
+		Config		config(configFile);
 		WebServer	server;
 		server.startServer();
 		server.runServer();
