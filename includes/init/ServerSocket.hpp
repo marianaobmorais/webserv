8e407c8298d2b4279e8707ef71effbd01585f10b
Mariana Morais <mariaoli@student.42porto.com>
2025-09-17 18:11:51 +0100

    Moved files

---
+++ includes/init/ServerSocket.hpp
@@ -0,0 +1,28 @@
+#ifndef SERVERSOCKET_HPP
+# define SERVERSOCKET_HPP
+
+#include <string>
+#include <vector>
+#include "ClientConnection.hpp"
+
+class ServerSocket
+{
+	private:
+		int					_fd;
+
+		ServerSocket(ServerSocket const& src); //memmove?
+		ServerSocket&					operator=(ServerSocket const& rhs); //memmove?
+	public:
+		ServerSocket(void);
+		~ServerSocket(void);
+
+		void				startSocket(std::string const& port);
+		void				listenConnections(int backlog);
+		std::vector<int>	acceptConnections(void);
+		//std::vector<ClientConnection>	acceptConnections(void); //accepts clients, producing a vector of ClientConnection
+
+		//accesor
+		int					getFD(void);
+};
+
+#endif //SERVERSOCKET_HPP
