6e47029ccd536d64efd12a53631848ca66dc50fd
Mariana Morais <mariaoli@student.42porto.com>
2025-09-17 18:11:34 +0100

    Modified getREsponseBuffer() for debugging

---
+++ includes/init/ClientConnection.hpp
@@ -0,0 +1,40 @@
+#ifndef CLIENTCONNECTION_HPP
+# define CLIENTCONNECTION_HPP
+
+#include <string>
+#include <sys/types.h>
+#include <request/HttpRequest.hpp>
+#include <response/HttpResponse.hpp>
+
+class ClientConnection
+{
+	private:
+		int					_fd;
+		std::string			_requestBuffer;
+		std::string			_responseBuffer;
+		size_t				_sentBytes;
+		//time_t				_lastActive;
+		HttpRequest			_httpRequest;
+		HttpResponse		_httpResponse;
+
+		ClientConnection&	operator=(ClientConnection const& rhs); //memmove?
+	public:
+		ClientConnection(int fd);
+		ClientConnection(ClientConnection const& src); //memmove?
+		~ClientConnection(void);
+
+		ssize_t				recvData(void);
+		ssize_t				sendData(ClientConnection &client, size_t sent, size_t toSend);
+		bool				completedRequest(void);
+		void				clearBuffer(void);
+
+		//accessors
+		int const&			getFD(void) const;
+		size_t const&		getSentBytes(void) const;
+		std::string const&	getRequestBuffer(void) const;
+		std::string /* const& */	getResponseBuffer(void) /* const */;
+		void				setSentBytes(size_t bytes);
+		void				setResponseBuffer(std::string buffer);
+};
+
+#endif //CLIENTCONNECTION_HPP
