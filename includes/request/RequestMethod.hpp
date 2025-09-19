633a47b1a78c660e298651f429fc295de8671214
Jose Junior <jose_evilasio@outlook.com>
2025-09-19 11:14:10 +0100

    added brief

--- includes/request/RequestMethod.hpp
+++ includes/request/RequestMethod.hpp
@@ -1,9 +1,27 @@
 #ifndef REQUEST_METHOD_HPP
 #define REQUEST_METHOD_HPP
 
+/**
+ * @struct RequestMethod
+ * @brief Defines supported HTTP request methods.
+ *
+ * This struct contains an enumeration of HTTP methods,
+ * as defined in RFC 2616 and its updates.
+ *
+ * Supported methods:
+ * - OPTIONS: Request for communication options.
+ * - GET: Retrieve data from the server.
+ * - HEAD: Same as GET but returns only headers.
+ * - POST: Submit data to the server (e.g., form submission).
+ * - PUT: Upload or replace a resource.
+ * - DELETE: Delete a resource.
+ * - TRACE: Echo the received request.
+ * - CONNECT: Establish a tunnel to the server.
+ * - PATCH: Apply partial modifications to a resource.
+ * - INVALID: Used to represent an unrecognized or unsupported method.
+ */
 struct RequestMethod
 {
-	//RFC 2616 5.1.1
 	enum Method
 	{
 		OPTIONS = 0,
--- includes/request/RequestState.hpp
+++ includes/request/RequestState.hpp
@@ -1,6 +1,19 @@
 #ifndef REQUEST_STATE_HPP
 #define REQUEST_STATE_HPP
 
+/**
+ * @struct RequestState
+ * @brief Represents the parsing state of an HTTP request.
+ *
+ * This struct defines the different stages involved in the
+ * parsing of an HTTP request.
+ *
+ * States:
+ * - RequestLine: Currently parsing the request line (method, URI, version).
+ * - Headers: Currently parsing the headers.
+ * - Body: Currently reading the message body (if present).
+ * - Complete: The request has been fully parsed.
+ */
 struct RequestState
 {
 	enum state
